0556f069a8659f75457a422a64c2e6d65be4d7a0
MK1499 <m.khaled1499@gmail.com>
2020-05-16 18:04:09 +0200

    add firebase

--- android/app/build/generated/source/buildConfig/debug/com/mk14/movielab/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/mk14/movielab/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.elmovie;
+package com.mk14.elmovie;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
